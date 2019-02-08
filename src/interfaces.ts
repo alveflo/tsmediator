0adc01830d53eaba5b9d07202902c0966e719085
willy.ovalle@klarna.com <willy.ovalle@klarna.com>
2019-02-08 17:37:10 +0100

    feat: initial RegisterHandler support

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -8,3 +8,12 @@ export interface IMediatorMiddleware {
     PreProcess: (request: any) => void;
     PostProcess: (request: any, response: any) => void;
 }
+
+// TODO: Proper types
+type IHandler = Function;
+type IRegisteredHandler = Function;
+
+export interface IContainer {
+    readonly handlers: { [id: string]: IHandler},
+    readonly registeredHandlers: { [id: string]: IRegisteredHandler }
+}
